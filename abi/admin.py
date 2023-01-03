0cd0831db32ab6b8939d0af1c3407b06b2d0d7de
AlexPakhomchik <korsar7772@gmail.com>
2023-01-03 11:59:46 +0300

    add db materials

--- abi/admin.py
+++ abi/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from abi.models import Materials
+
+admin.site.register(Materials)
 # Register your models here.
--- abi/models.py
+++ abi/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
+class Materials(models.Model):
+    type_of_material = models.CharField(max_length=200)
+    amount = models.IntegerField()
 # Create your models here.
